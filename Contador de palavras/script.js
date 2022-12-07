cf87ab0008e9c13bd3fdb1bab57a83732a642074
Henrique Ferreira <HneFer@hotmail.com>
2022-12-07 15:21:23 -0300

    Estudos novos de form me fizeram mudar de ideia

--- Contador de palavras/script.js
+++ Contador de palavras/script.js
@@ -22,3 +22,43 @@ function toggleHideModal(){
 // ------------------------
 // label sobre o contador
 
+/*const textField = document.querySelector("#text-field")
+const letters = document.querySelector("#letters")
+const vogals = document.querySelector("#vogals")
+const consoants = document.querySelector("#consoants")
+
+class CountDown {
+    constructor(letters, vogals, consoants){
+        this.letters = letters
+        this.vogals = vogals
+        this.consoants = consoants
+    }
+
+    countThem(keyWord){
+        let resultVogal = +this.vogals.innerText
+        let resultConso = +this.consoants.innerText
+        let resultAll = 0
+        const comparison = ['a', 'e', 'i', 'o', 'u']
+
+        if(!comparison.includes(keyWord)){
+            resultConso += 1
+            resultAll += 1
+            this.consoants.innerText = `${resultConso}`
+            this.letters.innerText = `${resultAll}`
+        } else {
+            resultVogal += 1
+            resultAll += 1
+            this.vogals.innerText = `${resultVogal}`
+            this.letters.innerText = `${resultAll}`
+        }
+    }
+}
+
+const counter = new CountDown(letters, vogals, consoants)
+
+textField.addEventListener("keydown", (elemento) => {
+    const catchKey = textField.value.length
+    
+    counter.countThem(catchKey)
+})*/
+
